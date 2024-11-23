897275ac67b5e49fc09d5430683e4438bbca23bb
thtech1 <roquelima182@gmail.com>
2024-11-23 20:55:15 -0300

    correção

--- script.js
+++ script.js
@@ -3,27 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const prevButton = document.querySelector(".prev");
     const nextButton = document.querySelector(".next");
     let currentSlide = 0;
-  
-    // Atualiza a visibilidade dos slides
+
     function updateSlide() {
       slides.forEach((slide, index) => {
         slide.classList.toggle("active", index === currentSlide);
       });
     }
   
-    // Botão Anterior
     prevButton.addEventListener("click", () => {
       currentSlide = (currentSlide === 0) ? slides.length - 1 : currentSlide - 1;
       updateSlide();
     });
   
-    // Botão Próximo
     nextButton.addEventListener("click", () => {
       currentSlide = (currentSlide + 1) % slides.length;
       updateSlide();
     });
-  
-    // Inicializa o slide ativo
+
     updateSlide();
   });
   
